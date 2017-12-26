8bec9dba0fefbfe6921e3b57ab3309cf9a262b8f
John Duncan <john@johnduncanphoto.com>
2017-12-26 17:13:43 +0000

    can now show word occurance from all houses

--- client/public/bundle.js
+++ client/public/bundle.js
@@ -75,12 +75,12 @@ const app = function () {
   const container = document.querySelector('#root');
   const houseInfoView = new HouseInfoView(container);
   const ajax = new Ajax();
-  allOcurances = {};
 
 
 
   const button = document.querySelector('#more-houses');
   button.addEventListener('click', function() {
+     let allOcurances = {};
     let pageNumber = 1;
     while (pageNumber <= 8) {
       pageNumber++;
@@ -91,6 +91,56 @@ const app = function () {
         console.log('allOcurances', allOcurances);
         // onePage = houseInfoView.chartPopulator(wordCount);
         // console.log('onepage',onePage);
+        Highcharts.chart('container', {
+          chart: {
+            type: 'column'
+          },
+          title: {
+            text: 'Most popular words in all House Words'
+          },
+          subtitle: {
+            text: ''
+          },
+          xAxis: {
+            type: 'category',
+            labels: {
+              rotation: -45,
+              style: {
+                fontSize: '8px',
+                fontFamily: 'Verdana, sans-serif'
+              }
+            }
+          },
+          yAxis: {
+            min: 0,
+            title: {
+              text: 'Word Count'
+            }
+          },
+          legend: {
+            enabled: false
+          },
+          tooltip: {
+            pointFormat: 'Word Occurs: <b>{point.y:1f} times</b>'
+          },
+          series: [{
+            name: 'Words',
+            data: houseInfoView.chartPopulator(allOcurances)
+            ,
+            dataLabels: {
+              enabled: false,
+              rotation: -90,
+              color: 'red',
+              align: 'right',
+              format: '{point.y:.1f}', // one decimal
+              y: 10, // 10 pixels down from the top
+              style: {
+                fontSize: '8px',
+                fontFamily: 'Verdana, sans-serif'
+              }
+            }
+          }]
+        })
       })
     }
 
@@ -98,56 +148,9 @@ const app = function () {
 
 
 
-  Highcharts.chart('container', {
-    chart: {
-      type: 'column'
-    },
-    title: {
-      text: 'Most popular words in all House Words'
-    },
-    subtitle: {
-      text: ''
-    },
-    xAxis: {
-      type: 'category',
-      labels: {
-        rotation: -45,
-        style: {
-          fontSize: '8px',
-          fontFamily: 'Verdana, sans-serif'
-        }
-      }
-    },
-    yAxis: {
-      min: 0,
-      title: {
-        text: 'Word Count'
-      }
-    },
-    legend: {
-      enabled: false
-    },
-    tooltip: {
-      pointFormat: 'Word Occurs: <b>{point.y:1f} times</b>'
-    },
-    series: [{
-      name: 'Words',
-      data: houseInfoView.chartPopulator(allOcurances)
-      ,
-      dataLabels: {
-        enabled: false,
-        rotation: -90,
-        color: 'red',
-        align: 'right',
-        format: '{point.y:.1f}', // one decimal
-        y: 10, // 10 pixels down from the top
-        style: {
-          fontSize: '8px',
-          fontFamily: 'Verdana, sans-serif'
-        }
-      }
-    }]
-  })
+
+
+
 
 
 }
