3093321b5226c3cc05f4b24625aaa48ce05b00c2
John Duncan <john@johnduncanphoto.com>
2017-12-26 16:42:14 +0000

    lodash installed and key value of hosue words is now added to rather than overwritten

--- client/src/app.js
+++ client/src/app.js
@@ -1,27 +1,28 @@
 const HouseInfoView = require('./views/house_info_view.js');
 const Ajax = require('./services/ajax.js');
+var _ = require('lodash');
 
 const app = function () {
   const container = document.querySelector('#root');
   const houseInfoView = new HouseInfoView(container);
-  graphData = new Array();
-
-  // const houseInfoView = new HouseInfoView();
-
-  // const url = `https://www.anapioficeandfire.com/api/houses?page=${pageNumber}&pageSize=50`;
   const ajax = new Ajax();
+
 
   const button = document.querySelector('#more-houses');
   button.addEventListener('click', function() {
+    let allOcurances = {};
     let pageNumber = 1;
     while (pageNumber <= 8) {
       pageNumber++;
       ajax.get(`https://www.anapioficeandfire.com/api/houses?page=${pageNumber}&pageSize=50`, function(data) {
         wordCount = houseInfoView.wordCount(data);
-        onePage = houseInfoView.chartPopulator(wordCount);
-        console.log(onePage);
-        graphData.push(onePage);
+        _.merge(allOcurances, wordCount)
+        console.log('wordcount',wordCount);
+        console.log('allOcurances', allOcurances);
+        // onePage = houseInfoView.chartPopulator(wordCount);
+        // console.log('onepage',onePage);
       })
+
     }
   })
 }
